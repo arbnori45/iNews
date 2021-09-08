50267fcfbe07f6f34095fa4b42abe96a95d4166a
Arbnor Tefiki <arbnori45@gmail.com>
2021-09-08 11:33:28 +0200

    Initial Commit

---
+++ Shared/TopNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopNewsApp.swift
+//  Shared
+//
+//  Created by Arbnor Tefiki on 8.9.21.
+//
+
+import SwiftUI
+
+@main
+struct TopNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
